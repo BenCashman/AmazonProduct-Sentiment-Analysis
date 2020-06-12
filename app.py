d933ccbc578b5069b94f346cfc8bc1d2999e8b17
BenCashman <ben.w.cashman@gmail.com>
2020-06-12 16:24:16 -0700

    Version 2 of app
    Here we are beginning to fill in dependencies as well as building out out Streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import torch
+import transformers
 
+def main():
 
+    st.title('Sentiment analysis and product reviews')
+    st.sidebar.title("Sentiment Analysis Web App")
+    st.sidebar.markdown("😃Is your review positive or negative?😞")
 
-st.title('Sentiment analysis and product reviews')
+if __name__ == '__main__':
+    main()
